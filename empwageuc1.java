88c1f0db9e5bea2757f007e61da21c3f114af836
Yugi07 <81014645+Yugi07@users.noreply.github.com>
2021-04-15 17:28:29 +0530

    Add files via upload

---
+++ empwageuc1.java
@@ -0,0 +1,7 @@
+public class empwagebuilderuc1 {
+  int IS_FULL_TIME = 1;
+double empcheck = math.Floor(Math.random() * 10) % 2;
+if (empcheck == IS_FULL_TIME)
+system.out.println("employee is present");
+else
+system.out.println("employee is absent");
